e3dd9773a750dd765c5806db9d990124d12bdee5
kalinstoev <kalinstoev@fan.gov>
2020-11-24 17:50:08 +0200

    Added Settings View Models

---
+++ SnkrsBank.Web/ViewModels/Settings/SettingViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SnkrsBank.Web.ViewModels.Settings
+{
+    public class SettingViewModel /*: IMapFrom<Setting>*/
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
