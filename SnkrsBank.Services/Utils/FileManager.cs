d738ad400f664e9ea392741e9fdf98d82590a3eb
kalinstoev <kalinstoev@fan.gov>
2020-11-24 17:01:04 +0200

    Added Utilitiesfor File Manager and Slug Generator

---
+++ SnkrsBank.Services/Utils/FileManager.cs
@@ -0,0 +1,39 @@
+﻿namespace SnkrsBank.Services.Utils
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public static class FileManager
+    {
+        public static async Task<string> SaveFile(IHostingEnvironment environment, IFormFile file)
+        {
+            var uploads = Path.Combine(environment.WebRootPath, "uploads");
+
+            if (file.Length > 0)
+            {
+                var filePath = Path.Combine(uploads, Guid.NewGuid().ToString(), file.FileName);
+
+                Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+
+
+                    return filePath;
+                }
+            }
+
+            return null;
+        }
+
+        public static string GetRelativeFilePath(string absolutePath)
+        {
+            return Path.Combine(Path.DirectorySeparatorChar.ToString(), "uploads", Path.GetFileName(Path.GetDirectoryName(absolutePath)), Path.GetFileName(absolutePath));
+        }
+    }
+}
---
+++ SnkrsBank.Services/Utils/SlugGenerator.cs
@@ -0,0 +1,43 @@
+﻿namespace SnkrsBank.Services.Utils
+{
+    using System.Globalization;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public static class SlugGenerator
+    {
+        public static string GenerateSlug(string phrase)
+        {
+            string str = RemoveDiacritics(phrase).ToLower();
+
+            // invalid chars
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", string.Empty);
+
+            // convert multiple spaces into one space
+            str = Regex.Replace(str, @"\s+", " ").Trim();
+
+            // cut and trim
+            str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+            str = Regex.Replace(str, @"\s", "-"); // hyphens
+
+            return str;
+        }
+
+        private static string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
