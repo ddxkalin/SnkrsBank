2e4bbed8bb80270ecbc1a8891f1dee62bd7518dd
kalinstoev <kalinstoev@fan.gov>
2020-11-26 01:59:46 +0200

    Added Admin Area and Created Admin Identity Pages for Account with VM's and cshtml views

---
+++ SnkrsBank.Web/Areas/Identity/Pages/Account/InputModels/RegisterInputModel.cs
@@ -0,0 +1,29 @@
+﻿namespace SnkrsBank.Web.Areas.Identity.Pages.Account.InputModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class RegisterInputModel
+    {
+        [Required(ErrorMessage = "The field is required")]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field is required")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "The field is required")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "The field is required")]
+        public string Lastname { get; set; }
+    }
+}
