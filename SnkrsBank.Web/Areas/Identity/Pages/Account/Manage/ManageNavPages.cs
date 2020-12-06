20cc00bf220e247f86ab213f3b98083af1676831
kalinstoev <kalinstoev@fan.gov>
2020-12-07 01:26:09 +0200

    Fixed all identity models and views

--- SnkrsBank.Web/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ SnkrsBank.Web/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -1,12 +1,16 @@
-﻿namespace SnkrsBank.Web.Areas.Identity.Pages.Account.Manage
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SnkrsBank.Web.Areas.Identity.Pages.Account.Manage
 {
-    using System;
-
-    using Microsoft.AspNetCore.Mvc.Rendering;
-
     public static class ManageNavPages
     {
         public static string Index => "Index";
+
+        public static string Email => "Email";
 
         public static string ChangePassword => "ChangePassword";
 
@@ -18,11 +22,11 @@
 
         public static string PersonalData => "PersonalData";
 
-        public static string ManagePosts => "ManagePosts";
-
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+
+        public static string EmailNavClass(ViewContext viewContext) => PageNavClass(viewContext, Email);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
 
@@ -36,9 +40,7 @@
 
         public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
 
-        public static string ManagePostsNavClass(ViewContext viewContext) => PageNavClass(viewContext, ManagePosts);
-
-        public static string PageNavClass(ViewContext viewContext, string page)
+        private static string PageNavClass(ViewContext viewContext, string page)
         {
             var activePage = viewContext.ViewData["ActivePage"] as string
                 ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
