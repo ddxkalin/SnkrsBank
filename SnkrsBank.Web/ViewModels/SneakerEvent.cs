b93dbd998848ccba54c1a8a7d215a085e3c22627
kalinstoev <kalinstoev@fan.gov>
2020-12-07 01:27:06 +0200

    Fixed SneakerEvent View Model

--- SnkrsBank.Web/ViewModels/SneakerEvent.cs
+++ SnkrsBank.Web/ViewModels/SneakerEvent.cs
@@ -4,19 +4,14 @@
 
     public class SneakerEvent
     {
-        [JsonProperty("id")]
         public string Id { get; set; }
 
-        [JsonProperty("title")]
         public string Title { get; set; }
 
-        [JsonProperty("description")]
         public string Description { get; set; }
 
-        [JsonProperty("start")]
         public string Start { get; set; }
 
-        [JsonProperty("end")]
         public string End { get; set; }
     }
 }
